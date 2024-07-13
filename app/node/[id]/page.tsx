20fd87f1328cd52bcfb580529096f0b97d9a0c7e
yongen.loong <yongen.loong@aelf.io>
2024-07-13 16:51:04 +0800

    feat: redirect for search

--- app/node/[id]/page.tsx
+++ app/node/[id]/page.tsx
@@ -1,51 +1,18 @@
-import Renderer, { AnyItem } from "@/components/blocks/renderer";
-import { fetcher } from "../../../lib/api";
-import GithubSlugger from "github-slugger";
-import TableOfContents from "@/components/blocks/table-of-contents";
-import { PrevNext } from "@/components/prev-next";
+import { getNode } from "@/services/get-node";
+import { toKebabCase } from "@/lib/utils";
+import { redirect } from "next/navigation";
 
-interface NodeData {
-  node: {
-    creator: string;
-    has_child: boolean;
-    node_create_time: string;
-    node_creator: string;
-    node_token: string;
-    node_type: string;
-    obj_create_time: string;
-    obj_edit_time: string;
-    obj_token: string;
-    obj_type: string;
-    origin_node_token: string;
-    origin_space_id: string;
-    owner: string;
-    parent_node_token: string;
-    space_id: string;
-    title: string;
-  };
-}
+async function getData(id: string, path: string[] = []) {
+  const node = await getNode(id);
+  const parentId = node.data.node.parent_node_token;
 
-interface DocData {
-  has_more: boolean;
-  items: AnyItem[];
-}
-
-async function getData(id: string) {
-  const { data } = await fetcher(
-    `https://open.larksuite.com/open-apis/wiki/v2/spaces/get_node?obj_type=wiki&token=${id}`
-  );
-
-  const node = data as NodeData;
-
-  if (node?.node?.obj_type === "docx") {
-    const id = node.node.obj_token;
-
-    const { data } = await fetcher(
-      `https://open.larksuite.com/open-apis/docx/v1/documents/${id}/blocks?document_revision_id=-1&page_size=500`
-    );
-    return data as DocData;
+  if (parentId) {
+    return await getData(parentId, [
+      toKebabCase(node.data.node.title),
+      ...path,
+    ]);
   } else {
-    throw new Error("not supported");
+    return ["/wiki", toKebabCase(node.data.node.title), ...path].join("/");
   }
 }
 
@@ -54,31 +21,7 @@ export default async function Document({
 }: {
   params: { id: string };
 }) {
-  const data = await getData(id);
-  const slugger = new GithubSlugger();
+  const path = await getData(id);
 
-  return (
-    <main className="flex overflow-x-hidden">
-      <div className="w-2/3">
-        {data?.items.map((item: AnyItem) => (
-          <Renderer
-            key={item.block_id}
-            {...item}
-            allItems={data.items}
-            slugger={slugger}
-          />
-        ))}
-        {/* <PrevNext /> */}
-        {process.env.NODE_ENV === "development" && (
-          <pre className="mt-5">
-            For developer use, only visible in development <br />
-            {JSON.stringify(data.items, undefined, 2)}
-          </pre>
-        )}
-      </div>
-      <aside className="w-1/3">
-        <TableOfContents allItems={data.items} />
-      </aside>
-    </main>
-  );
+  redirect(path);
 }
