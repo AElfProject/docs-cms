9aab3c131c82e26191beeb11a3473b217758aa7c
yongen.loong <yongen.loong@aelf.io>
2024-07-13 23:17:37 +0800

    feat: typesense search

--- components/search.tsx
+++ components/search.tsx
@@ -1,23 +1,66 @@
 "use client";
 
+import { useCallback, useEffect, useRef, useState } from "react";
+import { createPortal } from "react-dom";
+
 import {
-  CommandDialog,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command";
-import { useEffect, useState } from "react";
+  DocSearchModal,
+  DocSearchProps,
+  DocSearchButton,
+  useDocSearchKeyboardEvents,
+} from "typesense-docsearch-react";
+
+import "typesense-docsearch-css";
+
+const docSearchConfig: DocSearchProps = {
+  typesenseCollectionName: process.env.NEXT_PUBLIC_DOCSEARCH_INDEX_NAME!,
+  typesenseSearchParameters: {},
+  typesenseServerConfig: {
+    nodes: [
+      {
+        host: process.env.NEXT_PUBLIC_DOCSEARCH_APP_HOST!,
+        port: 443,
+        protocol: "https",
+      },
+    ],
+    apiKey: process.env.NEXT_PUBLIC_DOCSEARCH_API_KEY!,
+  },
+};
 
 export default function Search() {
-  const [open, setOpen] = useState(false);
+  const searchButtonRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [initialQuery, setInitialQuery] = useState<string | null>(null);
+
+  const onOpen = useCallback(() => {
+    setIsOpen(true);
+  }, [setIsOpen]);
+
+  const onClose = useCallback(() => {
+    setIsOpen(false);
+  }, [setIsOpen]);
+
+  const onInput = useCallback(
+    (event: KeyboardEvent) => {
+      setIsOpen(true);
+      setInitialQuery(event.key);
+    },
+    [setIsOpen, setInitialQuery]
+  );
+
+  useDocSearchKeyboardEvents({
+    isOpen,
+    onOpen,
+    onClose,
+    onInput,
+    searchButtonRef,
+  });
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
       if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
         e.preventDefault();
-        setOpen((open) => !open);
+        setIsOpen((open) => !open);
       }
     };
     document.addEventListener("keydown", down);
@@ -26,25 +69,12 @@ export default function Search() {
 
   return (
     <div className="w-full flex-1 md:w-auto md:flex-none">
-      <button
-        onClick={() => setOpen((open) => !open)}
-        className="inline-flex items-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input hover:bg-accent hover:text-accent-foreground px-4 py-2 relative h-8 w-full justify-start rounded-[0.5rem] bg-muted/50 text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-40 lg:w-64"
-      >
-        <span className="hidden lg:inline-flex">Search documentation...</span>
-        <span className="inline-flex lg:hidden">Search...</span>
-        <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
-          <span className="text-xs">⌘</span>K
-        </kbd>
-      </button>
-      <CommandDialog open={open} onOpenChange={setOpen}>
-        <CommandInput placeholder="Enter your query..." />
-        <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="Results">
-            <CommandItem>Example</CommandItem>
-          </CommandGroup>
-        </CommandList>
-      </CommandDialog>
+      <DocSearchButton ref={searchButtonRef} onClick={onOpen} />
+      {isOpen &&
+        createPortal(
+          <DocSearchModal {...docSearchConfig} initialScrollY={0} />,
+          document.body
+        )}
     </div>
   );
 }
