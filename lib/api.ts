66c796509a5812b37ab2123b3baddc5d208739af
AbigailDeng <Abigail.deng@ienyan.com>
2024-07-15 17:18:36 +0800

    feat: cache

--- lib/api.ts
+++ lib/api.ts
@@ -1,16 +1,18 @@
 "use server";
-
+import { cache } from "react";
 import { getTenantAccessToken } from "@/services/get-tenant-access-token";
 
-export const fetcher = async (url: string, next?: NextFetchRequestConfig) => {
-  const tenantAccessToken = await getTenantAccessToken();
-  const res = await fetch(url, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${tenantAccessToken}`,
-    },
-    next: next || { revalidate: 6000 },
-  });
+export const fetcher = cache(
+  async (url: string, next?: NextFetchRequestConfig) => {
+    const tenantAccessToken = await getTenantAccessToken();
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${tenantAccessToken}`,
+      },
+      next: next || { revalidate: 6000 },
+    });
 
-  return await res.json();
-};
+    return await res.json();
+  }
+);
