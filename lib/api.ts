81e7d87ab85b976fe8f0ea32f6a9bc543b09e689
yongen.loong <yongen.loong@aelf.io>
2024-07-16 14:47:16 +0800

    fix: exponential backoff

--- lib/api.ts
+++ lib/api.ts
@@ -1,29 +1,22 @@
 "use server";
 
 import { getTenantAccessToken } from "@/services/get-tenant-access-token";
+import { backOff } from "exponential-backoff";
 
 export const fetcher = async <T = any>(
   url: string,
   next?: NextFetchRequestConfig
 ): Promise<T> => {
-  if (process.env.CI) {
-    // throttle build in CI to avoid rate-limiting errors
-    await sleep();
-  }
-
   const tenantAccessToken = await getTenantAccessToken();
-  const res = await fetch(url, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${tenantAccessToken}`,
-    },
-    next: next || { revalidate: 6000 },
-  });
+  const res = await backOff(() =>
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${tenantAccessToken}`,
+      },
+      next: next || { revalidate: 6000 },
+    })
+  );
 
   return await res.json();
 };
-
-const sleep = async (delay = 500) =>
-  new Promise((resolve) => {
-    setTimeout(resolve, delay);
-  });
