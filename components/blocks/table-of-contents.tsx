2dd0691dae8032919951e6d7f1357d9ed9686551
yongen.loong <yongen.loong@aelf.io>
2024-07-23 15:35:06 +0800

    fix: table of contents

--- components/blocks/table-of-contents.tsx
+++ components/blocks/table-of-contents.tsx
@@ -13,60 +13,60 @@ export default function TableOfContents({
   const slugger = new GithubSlugger();
 
   return (
-    <ul className="list-none sm:p-8 p-1">
+    <ul className="list-none sm:p-4 p-1 border-l-2 ml-8">
       {allItems
-        .filter(i =>
+        .filter((i) =>
           [3, 4, 5, 6, 7, 8, 9, 10, 11]
-            .filter(i => (level ? i < level + 3 : true))
+            .filter((i) => (level ? i < level + 3 : true))
             .includes(i.block_type)
         )
-        .map(i => {
+        .map((i) => {
           let anchor = "";
 
           switch (i.block_type) {
             case 3:
               anchor = i.heading1.elements
-                .map(i => i.text_run.content)
+                .map((i) => i.text_run.content)
                 .join(" ");
               break;
             case 4:
               anchor = i.heading2.elements
-                .map(i => i.text_run.content)
+                .map((i) => i.text_run.content)
                 .join(" ");
               break;
             case 5:
               anchor = i.heading3.elements
-                .map(i => i.text_run.content)
+                .map((i) => i.text_run.content)
                 .join(" ");
               break;
             case 6:
               anchor = i.heading4.elements
-                .map(i => i.text_run.content)
+                .map((i) => i.text_run.content)
                 .join(" ");
               break;
             case 7:
               anchor = i.heading5.elements
-                .map(i => i.text_run.content)
+                .map((i) => i.text_run.content)
                 .join(" ");
               break;
             case 8:
               anchor = i.heading6.elements
-                .map(i => i.text_run.content)
+                .map((i) => i.text_run.content)
                 .join(" ");
               break;
             case 9:
               anchor = i.heading7.elements
-                .map(i => i.text_run.content)
+                .map((i) => i.text_run.content)
                 .join(" ");
               break;
             case 10:
               anchor = i.heading8.elements
-                .map(i => i.text_run.content)
+                .map((i) => i.text_run.content)
                 .join(" ");
               break;
             case 11:
               anchor = i.heading9.elements
-                .map(i => i.text_run.content)
+                .map((i) => i.text_run.content)
                 .join(" ");
               break;
           }
@@ -78,16 +78,19 @@ export default function TableOfContents({
           return (
             <li
               key={anchor}
-              className={clsx({
-                "ml-2": i.block_type === 4,
-                "ml-4": i.block_type === 5,
-                "ml-6": i.block_type === 6,
-                "ml-8": i.block_type === 7,
-                "ml-10": i.block_type === 8,
-                "ml-12": i.block_type === 9,
-                "ml-14": i.block_type === 10,
-                "ml-16": i.block_type === 11,
-              })}
+              className={clsx(
+                {
+                  "ml-[8px]": i.block_type === 4,
+                  "ml-[16px]": i.block_type === 5,
+                  "ml-[24px]": i.block_type === 6,
+                  "ml-[32px]": i.block_type === 7,
+                  "ml-[40px]": i.block_type === 8,
+                  "ml-[48px]": i.block_type === 9,
+                  "ml-[56px]": i.block_type === 10,
+                  "ml-[64px]": i.block_type === 11,
+                },
+                "text-[13px] leading-[21px]"
+              )}
             >
               <HighlightLink title={title} anchor={anchor} />
             </li>
